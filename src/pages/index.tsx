66c97eaae9791a4d04d35a44c6fc964ad369628f
Gabriel Davi <gdavi.gd@gmail.com>
2021-04-13 08:27:48 -0300

    create homepage structure

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,6 @@
 import { GetStaticProps } from 'next';
+import { AiOutlineCalendar, AiOutlineUser } from 'react-icons/ai';
+import Header from '../components/Header';
 
 import { getPrismicClient } from '../services/prismic';
 
@@ -24,9 +26,35 @@ interface HomeProps {
   postsPagination: PostPagination;
 }
 
-// export default function Home() {
-//   // TODO
-// }
+export default function Home(): JSX.Element {
+  return (
+    <div className={commonStyles.container}>
+      <Header />
+      {[1, 2, 3, 4, 5, 6].map((_, i) => (
+        <div className={styles.post}>
+          <h2>Como utilizar Hooks</h2>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime eius
+            deserunt placeat nulla vitae, quis ipsam ea animi sunt eaque!
+          </p>
+
+          <div>
+            <div>
+              <AiOutlineCalendar />
+              <time>15 mar 2021</time>
+            </div>
+            <div>
+              <AiOutlineUser />
+              <span>Joseph Oliveira</span>
+            </div>
+          </div>
+        </div>
+      ))}
+
+      <a>Carregar mais posts</a>
+    </div>
+  );
+}
 
 // export const getStaticProps = async () => {
 //   // const prismic = getPrismicClient();
