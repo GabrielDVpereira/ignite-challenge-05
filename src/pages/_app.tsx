d1570268f1afa7a60413bc607aff13281530aca3
Gabriel Davi <gdavi.gd@gmail.com>
2021-04-18 22:30:43 -0300

    adding toast container

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,15 @@
 import { AppProps } from 'next/app';
+import { ToastContainer } from 'react-toastify';
 import '../styles/globals.scss';
+import 'react-toastify/scss/main.scss';
 
 function MyApp({ Component, pageProps }: AppProps): JSX.Element {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <ToastContainer />
+      <Component {...pageProps} />
+    </>
+  );
 }
 
 export default MyApp;
