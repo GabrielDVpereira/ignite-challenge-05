ccad24ebe8c9baf66c3df5f1defbc120519a3eaa
Gabriel Davi <gdavi.gd@gmail.com>
2021-04-21 23:59:39 -0300

    post page

--- src/pages/post/[slug].tsx
+++ src/pages/post/[slug].tsx
@@ -1,45 +1,100 @@
+/* eslint-disable @typescript-eslint/explicit-module-boundary-types */
+/* eslint-disable @typescript-eslint/explicit-function-return-type */
 import { GetStaticPaths, GetStaticProps } from 'next';
 
-import { getPrismicClient } from '../../services/prismic';
+import {
+  AiOutlineCalendar,
+  AiOutlineUser,
+  AiOutlineClockCircle,
+} from 'react-icons/ai';
+import { formatDate } from '../../../utils/formating';
+import { getPostByUid, Post } from '../../services/postService';
 
 import commonStyles from '../../styles/common.module.scss';
 import styles from './post.module.scss';
 
-interface Post {
-  first_publication_date: string | null;
-  data: {
-    title: string;
-    banner: {
-      url: string;
-    };
-    author: string;
-    content: {
-      heading: string;
-      body: {
-        text: string;
-      }[];
-    }[];
-  };
-}
-
-interface PostProps {
+export interface PostProps {
   post: Post;
 }
 
-// export default function Post() {
-//   // TODO
-// }
+const WORDS_PER_MINUTE = 200;
 
-// export const getStaticPaths = async () => {
-//   const prismic = getPrismicClient();
-//   const posts = await prismic.query(TODO);
+export default function PostPage({ post }: PostProps) {
+  const getTotalLengthBody = (body: { text: string }[]): number => {
+    return body.reduce((total, { text }) => {
+      return total + text.split(' ').length;
+    }, 0);
+  };
 
-//   // TODO
-// };
+  const calculateTimeReading = () => {
+    const totalWords = post.data.content.reduce(
+      (totalContent, content) =>
+        totalContent +
+        content.heading.split(' ').length +
+        getTotalLengthBody(content.body),
+      0
+    );
 
-// export const getStaticProps = async context => {
-//   const prismic = getPrismicClient();
-//   const response = await prismic.getByUID(TODO);
+    return Math.ceil(totalWords / WORDS_PER_MINUTE);
+  };
 
-//   // TODO
-// };
+  return (
+    <>
+      <img className={styles.banner} src={post.data.banner.url} alt="banner" />
+      <div className={commonStyles.container}>
+        <div className={styles.post}>
+          <h1>{post.data.title}</h1>
+          <div className={styles.postInfo}>
+            <span>
+              <AiOutlineCalendar /> {formatDate(post.first_publication_date)}
+            </span>
+            <span>
+              <AiOutlineUser /> {post.data.author}
+            </span>
+            <span>
+              <AiOutlineClockCircle />
+              {calculateTimeReading()} min
+            </span>
+          </div>
+
+          <div className={styles.postContent}>
+            {post.data.content.map(section => (
+              <div key={section.heading}>
+                <h2>{section.heading}</h2>
+                {section.body.map(paragraph => (
+                  <p>{paragraph.text}</p>
+                ))}
+              </div>
+            ))}
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export const getStaticPaths: GetStaticPaths = async () => {
+  // const prismic = getPrismicClient();
+  // const posts = await prismic.query(TODO);
+
+  return {
+    paths: [],
+    fallback: 'blocking',
+  };
+
+  // TODO
+};
+
+export const getStaticProps: GetStaticProps = async context => {
+  const {
+    params: { slug },
+  } = context;
+
+  const post = await getPostByUid(String(slug));
+
+  // // TODO
+
+  return {
+    props: { post },
+  };
+};
