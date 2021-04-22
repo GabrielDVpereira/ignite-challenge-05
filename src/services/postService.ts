890bd6ff6ff973f33b6848afc8ecf3124c969a11
Gabriel Davi <gdavi.gd@gmail.com>
2021-04-22 20:28:40 -0300

    wrapping up the challenge

--- src/services/postService.ts
+++ src/services/postService.ts
@@ -38,23 +38,5 @@ export async function getPaths(): Promise<StaticPostPaths> {
 export async function getPostByUid(slug: string): Promise<Post> {
   const prismic = getPrismicClient();
   const response = await prismic.getByUID('posts', slug, {});
-  const post = formatPostResponse(response);
-  return post;
-}
-
-function formatPostResponse(unformatedPost): Post {
-  return {
-    first_publication_date: unformatedPost.first_publication_date,
-    data: {
-      title: unformatedPost.data.title,
-      banner: {
-        url: unformatedPost.data.banner.url,
-      },
-      author: unformatedPost.data.author,
-      content: unformatedPost.data.content.map(content => ({
-        heading: content.heading,
-        body: content.body.map(body => ({ text: body.text })),
-      })),
-    },
-  };
+  return response;
 }
